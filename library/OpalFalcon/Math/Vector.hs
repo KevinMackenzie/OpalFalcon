3a853c47f9beaf431a3eda71df0d1708274ce940
KevinMackenzie <dev.kjmack305@gmail.com>
2019-09-06 18:51:39 -0400

    Revert "Revert "Vector uses "Data.Vector.Fixed"""
    This reverts commit 7d061815ee6c2eafbe2f902a01992707b8736e40.

--- library/OpalFalcon/Math/Vector.hs
+++ library/OpalFalcon/Math/Vector.hs
@@ -6,70 +6,19 @@ module OpalFalcon.Math.Vector where
 -- TODO: pattern matching is just a convenience that made writing the initial code easier.  We should instead use fixed-length, unboxed vectors for all built-in aliases (for primative data types) and allow the user to modify the contents to generate new vectors and to extract components of the vectors
 
 import Data.Word (Word8)
-import Control.Applicative(Applicative, (<*>), liftA2)
+import Data.Bits ((.&.))
+import qualified Data.Vector.Fixed as V
+import qualified Data.Vector.Fixed.Primitive as VPrim
 
--- Base typeclasses
-class (Foldable a, Applicative a) => Vector a where
-    placeholder :: a b -> a b
+type Vec2 = VPrim.Vec2
+type Vec3 = VPrim.Vec3
+type Vec4 = VPrim.Vec4
 
-constVec :: Vector a => b -> a b
-constVec = pure
-
--- Arithmetic operations on vectors
-(|+|) :: (Vector a, Num b) => a b -> a b -> a b
-(|-|) :: (Vector a, Num b) => a b -> a b -> a b
-(|*|) :: (Vector a, Num b) => a b -> a b -> a b
-
-(|+|) = liftA2 (+)
-(|-|) = liftA2 (-)
-(|*|) = liftA2 (*)
-
--- Approximately equal to with default epsilon
-(~=) :: (Vector a, Ord b, Fractional b) => a b -> a b -> Bool
-(~=) = approxEq 0.00001
-
--- Used to see if two vectors are approximately equal to 
---  handle floating-point errors
-approxEq :: (Vector a, Ord b, Fractional b) => b -> a b -> a b -> Bool
-approxEq e v0 v1 = foldr (&&) True $ liftA2 (\x0 x1 -> (abs $ x0-x1) < e) v0 v1
-
--- If we don't support pattern matching (like using vectors/arrays) we don't need 'data' declarations for each vector length
--- Higher-order types
-data Vec2 a = V2 a a
-    deriving (Show, Eq, Foldable, Functor)
-data Vec3 a = V3 a a a
-    deriving (Show, Eq, Foldable, Functor)
-data Vec4 a = V4 a a a a
-    deriving (Show, Eq, Foldable, Functor)
-
--- Instances for low-dimmension vectors
-instance Applicative Vec2 where
-    pure x = V2 x x
-    (<*>) (V2 fx fy) (V2 x y)  = (V2 (fx x) (fy y))
-instance Vector Vec2 where
-    placeholder = id
-
-instance Applicative Vec3 where
-    pure x = V3 x x x
-    (<*>) (V3 fx fy fz) (V3 x y z)  = (V3 (fx x) (fy y) (fz z))
-instance Vector Vec3 where
-    placeholder = id
-
-instance Applicative Vec4 where
-    pure x = V4 x x x x
-    (<*>) (V4 fx fy fz fw) (V4 x y z w)  = (V4 (fx x) (fy y) (fz z) (fw w))
-instance Vector Vec4 where
-    placeholder = id
-
--- Vector type specializations
 type Vec2d = Vec2 Double
 type Vec2i = Vec2 Int
 
 type Vec3d = Vec3 Double
 type Vec3i = Vec3 Int
-
-origin :: (Vector a, Num b) => a b
-origin = constVec $ fromInteger 0
 
 -- Specialized for homogeneous coords
 type Vec4d = Vec4 Double
@@ -79,32 +28,57 @@ type ColorRGBA = Vec4 Data.Word.Word8
 type ColorRGBf = Vec3 Float
 type ColorRGBAf = Vec4 Float
 
+constVec :: (V.Vector v a, VPrim.Prim a) => a -> v a
+constVec x = V.generate (\_ -> x)
+
+-- Arithmetic operations on vectors
+(|+|) :: (V.Vector a b, Num b) => a b -> a b -> a b
+(|-|) :: (V.Vector a b, Num b) => a b -> a b -> a b
+(|*|) :: (V.Vector a b, Num b) => a b -> a b -> a b
+
+(|+|) = V.zipWith (+)
+(|-|) = V.zipWith (-)
+(|*|) = V.zipWith (*)
+
+-- Approximately equal to with default epsilon
+(~=) :: (VPrim.Prim b, Ord b, Fractional b) => Vec3 b -> Vec3 b -> Bool
+(~=) = approxEq 0.00001
+
+-- TODO: this should handle vectors of any length
+-- Used to see if two vectors are approximately equal to 
+--  handle floating-point errors
+approxEq :: (VPrim.Prim b, Ord b, Num b) => b -> Vec3 b -> Vec3 b -> Bool
+approxEq e v0 v1 = (V.foldr (.&.) (1 :: Int) $ V.zipWith (\x0 x1 -> if (abs $ x0-x1) < e then 1 else 0) v0 v1) == 1 
+
+origin :: (V.Vector a b, VPrim.Prim b, Num b) => a b
+origin = constVec 0
+
 white :: ColorRGB
 white = constVec 255
 whitef :: ColorRGBf
 whitef = constVec 1.0
 
-gray :: (Vector a) => b -> a b
+gray :: (V.Vector a b, VPrim.Prim b) => b -> a b
 gray = constVec
 
-black :: (Num a) => Vec3 a
-black = constVec (fromInteger 0)
+black :: (V.Vector a b, VPrim.Prim b, Num b) => a b
+black = constVec 0
 
-xPos :: Vec3 a -> a
-yPos :: Vec3 a -> a
-zPos :: Vec3 a -> a
+xPos :: (VPrim.Prim a) => Vec3 a -> a
+yPos :: (VPrim.Prim a) => Vec3 a -> a
+zPos :: (VPrim.Prim a) => Vec3 a -> a
 
-xPos (V3 x _ _) = x
-yPos (V3 _ y _) = y
-zPos (V3 _ _ z) = z
+xPos v = v V.! 0
+yPos v = v V.! 1
+zPos v = v V.! 2
 
-mutX :: Vec3 a -> a -> Vec3 a
-mutY :: Vec3 a -> a -> Vec3 a
-mutZ :: Vec3 a -> a -> Vec3 a
+mutX :: (VPrim.Prim a) => Vec3 a -> a -> Vec3 a
+mutY :: (VPrim.Prim a) => Vec3 a -> a -> Vec3 a
+mutZ :: (VPrim.Prim a) => Vec3 a -> a -> Vec3 a
 
-mutX (V3 _ y z) x = V3 x y z
-mutY (V3 x _ z) y = V3 x y z
-mutZ (V3 x y _) z = V3 x y z
+mutX v x = V.mk3 x         (v V.! 1) (v V.! 2)
+mutY v y = V.mk3 (v V.! 0) y         (v V.! 2)
+mutZ v z = V.mk3 (v V.! 0) (v V.! 1) z
 
 -- (r, theta)
 -- type PolarCoords = Vec2d
@@ -117,65 +91,77 @@ mutZ (V3 x y _) z = V3 x y z
 -- toSphere (V3 x y z) = V3 (sqrt $ x*x + y*y + z*z) (atan x y) (atan z $ sqrt $ x*x + y*y)
 
 -- Multiply by scalar
-(*|) :: (Vector a, Num b) => b -> a b -> a b
-(*|) s = fmap (s*)
-(|*) :: (Vector a, Num b) => a b -> b -> a b
+(*|) :: (V.Vector a b, Num b) => b -> a b -> a b
+(*|) s = V.map (s*)
+(|*) :: (V.Vector a b, Num b) => a b -> b -> a b
 (|*) v s = s *| v
 -- Dot product
-(|.|) :: (Vector a, Num b) => a b -> a b -> b
-(|.|) v0 v1 = foldr (+) 0 $ v0 |*| v1
+(|.|) :: (V.Vector a b, Num b) => a b -> a b -> b
+(|.|) v0 v1 = V.foldr (+) 0 $ v0 |*| v1
 -- Negate
-negateVec :: (Vector a, Num b) => a b -> a b
-negateVec = fmap negate
+negateVec :: (V.Vector a b, Num b) => a b -> a b
+negateVec = V.map negate
 -- Promote* converts lower order vectors into higher order ones
-promote3 :: (Num a) => Vec2 a -> Vec3 a
-promote3 (V2 x y) = V3 x y 0
+promote3 :: (VPrim.Prim a, Num a) => Vec2 a -> Vec3 a
+promote3 v = V.mk3 (v V.! 0) (v V.! 1) 0
 -- Demote* converts higher order vectors into lower order ones
-demote4 :: Vec4 a -> Vec3 a
-demote4 (V4 x y z _) = V3 x y z
-demote3 :: Vec3 a -> Vec2 a
-demote3 (V3 x y _) = V2 x y
+demote4 :: VPrim.Prim a => Vec4 a -> Vec3 a
+demote4 v = V.mk3 (v V.! 0) (v V.! 1) (v V.! 2)
+demote3 :: VPrim.Prim a => Vec3 a -> Vec2 a
+demote3 v = V.mk2 (v V.! 0) (v V.! 1)
 -- Distance function
-distance :: (Vector a, Floating b) => a b -> a b -> b
+distance :: (V.Vector a b, Floating b) => a b -> a b -> b
 distance v0 v1 = mag $ v0 |-| v1
 -- Magnitude
-mag :: (Vector a, Floating b) => a b -> b
-mag v = sqrt $ foldr (+) 0 $ v |*| v
+mag :: (V.Vector a b, Floating b) => a b -> b
+mag v = sqrt $ V.foldr (+) 0 $ v |*| v
 -- Sum
-vecSum :: (Vector a, Fractional b, Foldable c) => c (a b) -> a b
+vecSum :: (V.Vector a b, VPrim.Prim b, Fractional b, Foldable c) => c (a b) -> a b
 vecSum = foldr (|+|) origin
 -- Average
-vecAverage :: (Vector a, Fractional b, Foldable c) => c (a b) -> a b
+vecAverage :: (V.Vector a b, VPrim.Prim b, Fractional b, Foldable c) => c (a b) -> a b
 vecAverage l = 
     let len = length l
     in  case len of
             0 -> origin
             _ -> (vecSum l) |* (1.0 / (fromInteger $ toInteger $ len))
 -- Normalize
-normalize :: (Vector a, Floating b) => a b -> a b
-normalize v = fmap ( / (mag v)) v
+normalize :: (V.Vector a b, VPrim.Prim b, Floating b) => a b -> a b
+normalize v = V.map ( / (mag v)) v
 -- Clamp
-clamp :: (Vector a, Ord b) => a b -> a b -> a b
-clamp = liftA2 (\x y -> if x < y then x else y)
+clamp :: (V.Vector a b, Ord b) => a b -> a b -> a b
+clamp = V.zipWith (\x y -> if x < y then x else y)
 -- Cross Product (only defined on vec3's)
-(|><|) :: (Num a) => (Vec3 a) -> (Vec3 a) -> (Vec3 a)
-(|><|) (V3 a1 a2 a3) (V3 b1 b2 b3) = (V3 (a2*b3-a3*b2) (a3*b1-a1*b3) (a1*b2-a2*b1))
+(|><|) :: (VPrim.Prim a, Num a) => (Vec3 a) -> (Vec3 a) -> (Vec3 a)
+(|><|) a b = 
+    let a1 = xPos a
+        a2 = yPos a
+        a3 = zPos a
+        b1 = xPos b
+        b2 = yPos b
+        b3 = zPos b
+    in  V.mk3 (a2*b3-a3*b2) (a3*b1-a1*b3) (a1*b2-a2*b1)
 
 toPixel :: ColorRGBf -> ColorRGB
-toPixel = fmap (round . (255*))
+toPixel = V.map (round . (255*))
 
 -- Functions for converting vectors to and from homogeneous coords
-fromHomo :: (Fractional a) => Vec4 a -> Vec3 a
-fromHomo (V4 x y z w) = V3 (x/w) (y/w) (z/w)
-toHomo :: (Num a) => Vec3 a -> a -> Vec4 a
-toHomo (V3 x y z) w = (V4 x y z w)
-toHomoPos :: (Num a) => Vec3 a -> Vec4 a
+fromHomo :: (VPrim.Prim a, Fractional a) => Vec4 a -> Vec3 a
+fromHomo v = 
+    let x = v V.! 0
+        y = v V.! 1
+        z = v V.! 2
+        w = v V.! 3
+    in  V.mk3 (x/w) (y/w) (z/w)
+toHomo :: (VPrim.Prim a, Num a) => Vec3 a -> a -> Vec4 a
+toHomo v w = V.mk4 (xPos v) (yPos v) (zPos v) w
+toHomoPos :: (VPrim.Prim a, Num a) => Vec3 a -> Vec4 a
 toHomoPos v = toHomo v 1
-toHomoDir :: (Num a) => Vec3 a -> Vec4 a
+toHomoDir :: (VPrim.Prim a, Num a) => Vec3 a -> Vec4 a
 toHomoDir v = toHomo v 0
 
 -- Reflection assumes 'norm' is normalized
 --  Note: 'v' is centered at the origin; 'incoming' vectors must be flipped
-reflect :: (Fractional a) => Vec3 a -> Vec3 a -> Vec3 a
+reflect :: (VPrim.Prim a, Fractional a) => Vec3 a -> Vec3 a -> Vec3 a
 reflect v norm = ((2 * (v |.| norm)) *| norm) |-| v
 
