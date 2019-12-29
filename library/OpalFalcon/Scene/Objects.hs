223ce3f69a1def133f16633a9c3258892e7a7cbc
KevinMackenzie <dev.kjmack305@gmail.com>
2019-12-29 00:32:57 -0500

    Added triangle objects

--- library/OpalFalcon/Scene/Objects.hs
+++ library/OpalFalcon/Scene/Objects.hs
@@ -1,40 +1,50 @@
 module OpalFalcon.Scene.Objects where
 
-import OpalFalcon.Math.Vector
-import OpalFalcon.Math.Transformations
 import OpalFalcon.BaseTypes as Bt
-import OpalFalcon.Scene.Objects.Sphere as S
-import OpalFalcon.Scene.Objects.Plane as P
+import OpalFalcon.Math.Transformations
+import OpalFalcon.Math.Vector
 import OpalFalcon.Scene.Objects.Disc as D
-import OpalFalcon.Scene.Objects.PointLight as PL
 import OpalFalcon.Scene.Objects.DiscLight as DL
+import OpalFalcon.Scene.Objects.Plane as P
+import OpalFalcon.Scene.Objects.PointLight as PL
+import OpalFalcon.Scene.Objects.Sphere as S
+import OpalFalcon.Scene.Objects.Triangle as T
 
 -- TODO: this is not straightforward.  Make constructors from objects and their materials.  The separation is so we can have the same objects rendered with different materials so real-time interface can use simple materials?  Or maybe we can just use the 'diffuse' component of the material?
 
 -- Makes an object out of a sphere
 mkSphereObject :: Sphere -> SphereMat -> Bt.Object
-mkSphereObject sphere@(MkSphere space _) mat = 
-    Bt.MkObj { objPos = spacePos space
-             , objIntersectRay = S.hittestSphere sphere mat
-             }
+mkSphereObject sphere@(MkSphere space _) mat =
+  Bt.MkObj
+    { objPos = spacePos space,
+      objIntersectRay = S.hittestSphere sphere mat
+    }
 
 mkPlaneObject :: Plane -> PlaneMat -> Bt.Object
-mkPlaneObject plane@(MkPlane space) mat = 
-    Bt.MkObj { objPos = spacePos space
-             , objIntersectRay = P.hittestPlaneFront plane mat
-             }
+mkPlaneObject plane@(MkPlane space) mat =
+  Bt.MkObj
+    { objPos = spacePos space,
+      objIntersectRay = P.hittestPlaneFront plane mat
+    }
 
 mkDiscObject :: Disc -> DiscMat -> Bt.Object
 mkDiscObject disc@(MkDisc (MkPlane space) _) mat =
-    Bt.MkObj { objPos = spacePos space
-             , objIntersectRay = D.hittestDisc disc mat
-             }
+  Bt.MkObj
+    { objPos = spacePos space,
+      objIntersectRay = D.hittestDisc disc mat
+    }
+
+mkTriangleObject :: Triangle -> TriangleMat -> Bt.Object
+mkTriangleObject triangle mat =
+  Bt.MkObj
+    { objPos = T.trianglePos triangle,
+      objIntersectRay = T.hittestTriangleFront triangle mat
+    }
 
 mkDiscLight :: D.Disc -> ColorRGBf -> Float -> LightSource
-mkDiscLight d c p = 
-    MkLight { lightSample = DL.sampleDiscLight 10 (DL.MkDL d c p) }
+mkDiscLight d c p =
+  MkLight {lightSample = DL.sampleDiscLight 10 (DL.MkDL d c p)}
 
 mkPointLight :: Vec3d -> ColorRGBf -> Float -> LightSource
-mkPointLight p c d = 
-    MkLight { lightSample = PL.samplePointLight (PL.MkPL p c d) } 
-
+mkPointLight p c d =
+  MkLight {lightSample = PL.samplePointLight (PL.MkPL p c d)}
---
+++ library/OpalFalcon/Scene/Objects/Triangle.hs
@@ -0,0 +1,57 @@
+module OpalFalcon.Scene.Objects.Triangle
+  ( TriangleMat,
+    Triangle (MkTriangle),
+    triangleNorm,
+    trianglePos,
+    hittestTriangleFront,
+    hittestTriangle,
+  )
+where
+
+import OpalFalcon.BaseTypes
+import OpalFalcon.Math.Ray
+import OpalFalcon.Math.Transformations
+import OpalFalcon.Math.Vector
+
+-- Note: The parameter for triangular materials is in barycentric coords.  I think this lets us interpolate across UV parameters easily for textured triangles
+type TriangleMat = Triangle -> Vec3d -> AppliedMaterial
+
+-- A triangle is an ordered set of 3 points with CCW normal ordering
+data Triangle = MkTriangle !Vec3d !Vec3d !Vec3d
+
+trianglePos :: Triangle -> Vec3d
+trianglePos (MkTriangle v0 v1 v2) = (v0 |+| v1 |+| v2) |/ 3
+
+triangleNorm :: Triangle -> Vec3d
+triangleNorm (MkTriangle v0 v1 v2) = (v1 |-| v0) |><| (v2 |-| v0)
+
+-- Hittest that ignores backface hits
+hittestTriangleFront :: Triangle -> TriangleMat -> Ray -> Maybe Hit
+hittestTriangleFront t mat r@(Ray _ rDir) = if ((triangleNorm t) |.| rDir) > 0 then Nothing else hittestTriangle t mat r
+
+-- Hittests either side of a triangle
+-- https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/ray-triangle-intersection-geometric-solution
+hittestTriangle :: Triangle -> TriangleMat -> Ray -> Maybe Hit
+hittestTriangle t@(MkTriangle v0 v1 v2) mat r@(Ray rPos rDir) =
+  let n' = triangleNorm t
+      n = normalize n'
+      a2 = mag n' -- cross product is the half-area
+      d = n |.| v0 -- the constant offset of the implicit plane function
+      dDir = n |.| rDir
+      p = ((n |.| rPos) + d) / dDir
+      hPos = pointAtParameter r p
+      vc0 = (v1 |-| v0) |><| (hPos |-| v0)
+      vc1 = (v2 |-| v1) |><| (hPos |-| v1)
+      vc2 = (v0 |-| v2) |><| (hPos |-| v2)
+      u = (mag vc1) / a2
+      v = (mag vc2) / a2
+      w = (mag vc0) / a2
+   in if ((abs dDir) < 0.001) || (p < 0) || ((n |.| vc0) < 0) || ((n |.| vc1) < 0) || ((n |.| vc2) < 0)
+        then Nothing
+        else Just MkHit
+          { hitPos = hPos,
+            hitNorm = n,
+            hitInc = r,
+            hitParam = p,
+            hitMat = mat t (V3 u v w)
+          }
