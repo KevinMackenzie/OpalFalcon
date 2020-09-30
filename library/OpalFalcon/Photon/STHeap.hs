600bcef2839f6fe9aa48cb7edcdb9403df39f190
KevinMackenzie <dev.kjmack305@gmail.com>
2020-09-30 09:46:00 -0400

    STHeap uses storable vectors now

--- library/OpalFalcon/Photon/STHeap.hs
+++ library/OpalFalcon/Photon/STHeap.hs
@@ -13,89 +13,94 @@ where
 
 import Control.Monad
 import Control.Monad.ST
-import qualified Data.Array
-import Data.Array.ST
 import Data.STRef
+import qualified Data.Vector.Storable as VS
+import qualified Data.Vector.Storable.Mutable as VSM
 
--- TODO: This is a useful thing to have, but it is mostly a stop-gap until a more performant priority heap that operates on unboxed photons and avoids lifting until final density estimations are required
+-- TODO: replace all vector ops with unsafe ones once we know it is stable
+
+{-# INLINE readOneBased #-}
+readOneBased :: (PrimMonad m, Storable a) => MVector (PrimState m) a -> Int -> m a
+readOneBased vec idx = VSM.read vec (idx -1)
+
+{-# INLINE writeOneBased #-}
+writeOneBased :: (PrimMonad m, Storable a) => MVector (PrimState m) a -> Int -> a -> m ()
+writeOneBased vec idx = VSM.write vec (idx -1)
 
 -- This is a max-heap
-data STHeap s e p = STHeap (e -> p) Int (STRef s Int) (STArray s Int e)
+data STHeap s e p = STHeap (e -> p) Int (STRef s Int) (VSM.MVector s e)
 
-mkSTHeap :: (Ord p) => Int -> e -> (e -> p) -> ST s (STHeap s e p)
+mkSTHeap :: (Storable e, Ord p) => Int -> e -> (e -> p) -> ST s (STHeap s e p)
 mkSTHeap size blank priority = do
   sz <- newSTRef 0
-  arr <- newArray (1, size) blank
-  return $ STHeap priority size sz arr
+  vec <- VSM.new size
+  return $ STHeap priority size sz vec
 
-percolateUp :: (Ord p) => Int -> STHeap s e p -> ST s ()
+percolateUp :: (Storable e, Ord p) => Int -> STHeap s e p -> ST s ()
 percolateUp 0 _ = return ()
 percolateUp 1 _ = return ()
-percolateUp p h@(STHeap pr _ sz arr) =
+percolateUp p h@(STHeap pr _ sz vec) =
   let parentIdx = p `div` 2
    in do
-        val <- readArray arr p
-        pval <- readArray arr parentIdx
+        val <- readOneBased vec p
+        pval <- readOneBased vec parentIdx
         when ((pr val) > (pr pval)) $ do
-          writeArray arr parentIdx val
-          writeArray arr p pval
+          writeOneBased vec parentIdx val
+          writeOneBased vec p pval
           percolateUp parentIdx h
 
 -- NOTE: only call this on a full heap
-percolateDown :: (Ord p) => Int -> STHeap s e p -> ST s ()
-percolateDown p h@(STHeap pr cap _ arr) =
+percolateDown :: (Storable e, Ord p) => Int -> STHeap s e p -> ST s ()
+percolateDown p h@(STHeap pr cap _ vec) =
   let lChild = 2 * p
       rChild = 2 * p + 1
    in if lChild > cap
         then return ()
         else do
-          pVal <- readArray arr p
-          lVal <- readArray arr lChild
+          pVal <- readOneBased vec p
+          lVal <- readOneBased vec lChild
           (cIdx, cVal) <-
             if rChild > cap
               then return (lChild, lVal)
               else do
-                rVal <- readArray arr rChild
+                rVal <- readOneBased vec rChild
                 return $
                   if (pr lVal) > (pr rVal)
                     then (lChild, lVal)
                     else (rChild, rVal)
           when ((pr cVal) > (pr pVal)) $ do
-            writeArray arr p cVal
-            writeArray arr cIdx pVal
+            writeOneBased vec p cVal
+            writeOneBased vec cIdx pVal
             percolateDown cIdx h
 
 -- replaces highest priority item
-pushHeap :: (Ord p) => e -> STHeap s e p -> ST s ()
-pushHeap val h@(STHeap pr cap sz arr) = do
+pushHeap :: (Storable e, Ord p) => e -> STHeap s e p -> ST s ()
+pushHeap val h@(STHeap pr cap sz vec) = do
   size <- readSTRef sz
   if (size < cap)
     then do
       -- Until we hit capacity, fill top-down
       modifySTRef sz (+ 1)
-      writeArray arr (size + 1) val
+      writeOneBased vec (size + 1) val
       percolateUp (size + 1) h
     else do
       -- Once we hit capacity, replace the highest priority item
-      hval <- readArray arr 1
+      hval <- readOneBased vec 1
       when ((pr val) < (pr hval)) $ do
-        writeArray arr 1 val
+        writeOneBased vec 1 val
         percolateDown 1 h
 
-getHeapRoot :: (Ord p) => STHeap s e p -> ST s e
-getHeapRoot (STHeap _ _ _ arr) = readArray arr 1
+getHeapRoot :: (Storable e, Ord p) => STHeap s e p -> ST s e
+getHeapRoot (STHeap _ _ _ vec) = readOneBased vec 1
 
-getHeapRootPriority :: (Ord p) => STHeap s e p -> ST s p
+getHeapRootPriority :: (Storable e, Ord p) => STHeap s e p -> ST s p
 getHeapRootPriority h@(STHeap pr _ _ _) = (getHeapRoot h) >>= (return . pr)
 
-getHeapSize :: STHeap s e p -> ST s Int
+getHeapSize :: Storable e => STHeap s e p -> ST s Int
 getHeapSize (STHeap _ _ sz _) = readSTRef sz
 
-getHeapContents :: STHeap s e p -> ST s ([e], Int)
-getHeapContents (STHeap pr cap sz arr) = do
-  arrElems <- getElems arr
+getHeapContents :: (Storable e) => STHeap s e p -> ST s (VS.Vector e)
+getHeapContents (STHeap pr cap sz vec) = do
+  vecImmutable <- VSM.freeze vec
   numElems <- readSTRef sz
-  return (take numElems arrElems, numElems)
-
-runSTHeap :: (forall s. ST s (STHeap s e p)) -> [e]
-runSTHeap action = Data.Array.elems $ runSTArray $ (\(STHeap _ _ _ arr) -> arr) <$> action
+  return $ VS.take numElems vecImmutable
